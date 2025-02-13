3f1408ef5561ff7414b59ea9de9fc6e039faa341
akoua <akouayao225@gmail.com>
2025-02-13 06:27:32 +0100

    add comment

--- src/main/java/formation/afiuna/springboot/iod/ServiceA.java
+++ src/main/java/formation/afiuna/springboot/iod/ServiceA.java
@@ -4,7 +4,8 @@ public class ServiceA {
     private ServiceB serviceB;
 
     public ServiceA() {
-        //Couplage fort entre le serviceA et le serviceB
+        // Couplage fort entre le serviceA et le serviceB
+        // Le comportement souhaité se trouve sur la branche principe_injection_dependance_goal
         this.serviceB = new ServiceB(10, 1.2);
     }
 
