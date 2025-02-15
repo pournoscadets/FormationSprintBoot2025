888866ee20fc82bb16c684560269ba180cb8df64
akoua <akouayao225@gmail.com>
2025-02-15 12:30:10 +0100

    dip update

--- src/main/java/formation/afiuna/springboot/iod/DIP.java
+++ src/main/java/formation/afiuna/springboot/iod/DIP.java
@@ -1,6 +1,15 @@
+// Principe de Segregation
+//Principe de Responsabilité unique
 public interface IPaymentService {
     public double payment();
+    public double paymentInstanne();           
 }
+
+// Principe de Segregation
+public interface IRetraitService {
+    public double retrait();
+}
+
 
 public class CardPayment implements IPaymentService { 
     @Override   
@@ -9,7 +18,23 @@ public class CardPayment implements IPaymentService {
     }
 }
 
+public class WavePayment implements IPaymentService { 
+    @Override   
+    public double payment() {
+        return 2.0;
+    }
+}
+
+public class RetraitEspece implements IRetraitService { 
+    @Override   
+    public double retrait() {
+        return 1.0;
+    }
+}
+
+//Principe de Responsabilité unique
 public class PaymentService{
+    //Principe de substitution de Liskov
     private IPaymentService paymentService;
 
     // Injection de dépendance via le constructeur
@@ -20,11 +45,34 @@ public class PaymentService{
     public double executePayment(){ 
         return paymentService.payment();        
     }
+
+    public double executePaymentInstanee(){ 
+        return paymentService.paymentInstanne();        
+    }
 }
 
+//Principe de Responsabilité unique
+public class RetraitService{
+    //Principe de substitution de Liskov
+    private IRetraitService retraitService;
+
+    // Injection de dépendance via le constructeur
+    public RetraitService(IRetraitService retraitService) {
+        this.retraitService = retraitService;
+    }
+
+    public double executeRetrait(){ 
+        return retraitService.retrait();        
+    }
+}
 public class MainClass{
     public static void main(String[] args) {
-        PaymentService paymentService = new PaymentService(new CardPayment());
-        System.out.println(paymentService.executePayment());
+        
+        PaymentService paymentCardService = new PaymentService(new CardPayment());
+        //Principe de Open/Closed
+        PaymentService paymentWaveService = new PaymentService(new WavePayment());
+
+        RetraitService retraitService = new RetraitService(new RetraitEspece());
+        System.out.println(paymentWaveService.executePayment());
     }
 }
