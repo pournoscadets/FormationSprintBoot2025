426644b6d3795ba200402a35a45620e8aa8d42aa
akoua <akouayao225@gmail.com>
2025-02-28 23:27:33 +0100

    add a connection to database

--- src/main/java/formation/afiuna/springboot/SpringbootApplication.java
+++ src/main/java/formation/afiuna/springboot/SpringbootApplication.java
@@ -1,23 +1,13 @@
 package formation.afiuna.springboot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import formation.afiuna.springboot.iod.ServiceA;
 
 @SpringBootApplication
 public class SpringbootApplication {
 	
 	public static void main(String[] args) {				
-		
-		try (AnnotationConfigApplicationContext context =
-                  new AnnotationConfigApplicationContext("formation.afiuna.springboot")) {
-
-					ServiceA serviceA = context.getBean(ServiceA.class);
-					System.out.println(serviceA.calcule());
-    }			
-
-		//SpringApplication.run(SpringbootApplication.class, args);
+		SpringApplication.run(SpringbootApplication.class, args);
 	}	
 
 }
